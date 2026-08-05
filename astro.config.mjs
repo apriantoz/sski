6b1f19d45c13e334bc7344fae336bfe67e63a2b0
Apriantoz <ekaaprianta@gmail.com>
2026-08-05 15:01:44 +0800

    refactor: simplify astro.config.mjs by removing Netlify-specific configurations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,11 @@
-/* // @ts-check
+// @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://apriantoz.github.io',
-  base: '/sski',
-  vite: {
-    plugins: [tailwindcss()]
-  }
-}); */
-
-// @ts-check
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
-
-// Deteksi apakah sedang di-build oleh Netlify via import.meta.env
-const isNetlify = import.meta.env.NETLIFY === 'true';
-
-// https://astro.build/config
-export default defineConfig({
-  site: isNetlify ? 'https://sski-demo.netlify.app' : 'https://apriantoz.github.io',
-  base: isNetlify ? '/' : '/sski',
+  base: '/',
   vite: {
     plugins: [tailwindcss()]
   }
