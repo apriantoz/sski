50ee92b4aa6e39e4b259d96de489f93c8b4ec592
Apriantoz <ekaaprianta@gmail.com>
2026-08-05 15:43:21 +0800

    fix: update asset paths to include '/sski' prefix for consistency

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://apriantoz.github.io',
-  base: '/',
+  base: '/sski',
   vite: {
     plugins: [tailwindcss()]
   }
